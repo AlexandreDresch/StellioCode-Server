4a6fe263ec2747c682b86f9472a73018d1b690e9
AlexandreDresch <connectWithAlexandre@proton.me>
2024-12-11 23:11:29 -0300

    feat: add initial setup

---
+++ src/main/java/com/stelliocode/backend/StellioCodeApplication.java
@@ -0,0 +1,14 @@
+package com.stelliocode.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class StellioCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellioCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stelliocode/backend/StellioCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stelliocode.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellioCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
